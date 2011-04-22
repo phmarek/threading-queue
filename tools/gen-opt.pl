4aa7735e499d50b681df6541bee6e626dbac6de2
Philipp Marek <philipp+github@marek.priv.at>
2011-04-22 15:42:59 +0200

    Cleaned up and simplified option parsing a bit.

--- tools/gen-opt.pl
+++ tools/gen-opt.pl
@@ -1,72 +1,90 @@
 #!/usr/bin/perl
 
 
-
 $/ = "\n\n";
 
-@gl = @doc = @ps = @al = @ops = ();
+@gl = @odoc = @ps = @al = @ops = @doc = @opt = ();
 
 while (<>) {
 	last if /^\n*=head\d+ Global options/;
+    next if /^\n*=/;
+    chomp;
+    s/"/\\"/g;
+    push @doc, $_;
 }
 
-push @doc, "Global options:", "";
-while (<>) {
-	chomp;
-	last if /^\n*=back/;
 
-	if (/^=item (\S+) (\S+)$/) {
-		push @gl, "(cons $1 $2)";
-		push @doc, "** $1: ";
-	} else {
-		s/C<(\w+)>/$1/g;
-		$doc[-1] .= $_ . "\n" if @gl;
-	}
-}
+push @odoc, "Global options:", "";
+push @gl, ";; global";
+
+Parse();
 
 while (<>) {
 	last if (/\n*=head\d+ Per-step options/ .. /^=over/) =~ /E/;
 }
 
-push @doc, "", "Per-step options:", "";
-while (<>) {
-	chomp;
-	last if /^\n*=back/;
+push @odoc, "", "Per-step options:", "";
+push @gl, ";; per-step";
 
-	if (/^=item (\S+) (\S+)(?: aka (.*))?$/) {
-		$opt = $1;
-		push @ps, "(cons $1 $2)";
-		push @ops, $1;
-		push @doc, "** $1: ";
-		push @al, map { "($_ . $opt)"; } split(/[, ]+/, $3) if $3;
-	} else {
-		$doc[-1] .= $_ . "\n" if @ps;
-	}
-}
+Parse( sub { push @ops, $_[0]; } );
 
+
+## Output generation
 
 print <<EOT;
 ;; Generated file, do not edit!!
 
-(in-package :thread-safe-queue)
+(in-package :threading-queue)
 
 EOT
 
 print join("\n\t",
-	"(defconstant +all-options+ (list",
-	";; global",
+	"(define-constant +all-options+ (list",
 	@gl,
-	";; per-step",
-	@ps,
 	")",
-	'"', (map { s/\s+$//; $_; } @doc), 
+	":test #'equal :documentation \"", (map { s/\s+$//; $_; } @odoc),
 	'")'), "\n\n\n";
 
 
-print "(defconstant +per-stmt-options+ '(\n\t",
-	join(" ", @ops), "))\n\n";
+print "(define-constant +per-stmt-options+ '(\n\t",
+	join(" ", @ops), ")\n",
+    "\t:test #'equal)\n\n";
 
 print join("\n\t",
-	"(defconstant +option-aliases+ '(",
-	@al), "))\n\n";
+	"(define-constant +option-aliases+ '(",
+	@al), ")\n",
+    "\t:test #'equal)\n\n";
+
+print "(define-constant +threading-feed-doc+\n\"",
+    join("\n", @doc), "\n",
+    "Please see +all-options+ for more information about the options; the small reminder list is\n\t",
+    join(" ", @opt), "\"\n",
+    "\t:test #'equal)\n\n";
+
+exit;
+
+
+sub Parse
+{
+    my ($fn) = @_;
+    my $opt = 0;
+
+    while (<>) {
+        chomp;
+        last if /^\n*=back/;
+
+        if (/^=item (\S+) (.+?)(?: aka (.*))?\s*$/) {
+            $opt = $1;
+            push @opt, $1;
+            push @gl, "(cons $1 $2)";
+            &$fn($1, $2) if $fn;
+            push @odoc, "** $1: ";
+            push @al, map { "($_ . $opt)"; } split(/[, ]+/, $3) if $3;
+        } else {
+            s/C<(\w+)>/$1/g;
+            $odoc[-1] .= $_ . "\n" if $opt;
+        }
+    }
+}
+
 
